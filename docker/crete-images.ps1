8c6a6716313d1c83fdb86b76cb2c26ae396d1c63
Ivan Montilla <montyclt@outlook.com>
2022-04-20 19:11:14 +0200

    Fixed script

--- docker/crete-images.ps1
+++ docker/crete-images.ps1
@@ -1,3 +1,9 @@
-docker build . -t butterfly:latest-amd64 --file ./docker/Dockerfile-amd64
-docker build . -t butterfly:latest-arm64v8 --file ./docker/Dockerfile-arm64v8
-docker tag butterfly:latest-amd64 butterfly:latest
+param (
+    [Parameter(Mandatory = $true)] [string] $Version
+)
+
+docker build . -t iokode/butterfly:$Version-amd64 --file ./docker/Dockerfile-amd64
+docker build . -t iokode/butterfly:$Version-arm64v8 --file ./docker/Dockerfile-arm64v8
+docker tag iokode/butterfly:$Version-amd64 iokode/butterfly:latest-amd64
+docker tag iokode/butterfly:latest-amd64 iokode/butterfly:latest
+docker tag iokode/butterfly:$Version-arm64v8 iokode/butterfly:latest-arm64v8
